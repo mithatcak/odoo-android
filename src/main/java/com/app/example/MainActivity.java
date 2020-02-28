60e3a134878b337a1e42d1fcd30daae632ad54da
Mithat C <mithatcancakmak@gmail.com>
2020-02-27 23:00:56 -0500

    print on page load

--- src/main/java/com/app/example/MainActivity.java
+++ src/main/java/com/app/example/MainActivity.java
@@ -16,8 +16,25 @@ import android.webkit.WebViewClient;
 import android.widget.Toast;
 import android.webkit.CookieManager;
 
+
+/* printer libraries */
+import android.printer.sdk.util.PowerUtils;
+import android.printer.sdk.PosFactory;
+import cn.pda.serialport.SerialDriver;
+import android.printer.sdk.interfaces.IPosApi;
+import android.printer.sdk.interfaces.OnPrintEventListener;
+import android.printer.sdk.constant.BarCode;
+import android.printer.sdk.bean.BarCodeBean;
+import android.printer.sdk.bean.enums.ALIGN_MODE;
+
 public class MainActivity extends AppCompatActivity {
 
+    //printer stuff
+    private IPosApi mPosApi;
+    private int mConcentration=25;
+
+
+    //Webview stuff
     private WebView webView;
     ProgressDialog mProgressDialog;
     ProgressDialog progressDialog;
@@ -34,6 +51,9 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
 
         setContentView(R.layout.activity_main);
+
+        //Init Printer
+        initPos ();
 
         mProgressDialog = new ProgressDialog(this);
         mProgressDialog.setMessage("Loading... ");
@@ -59,6 +79,102 @@ public class MainActivity extends AppCompatActivity {
         webView.setWebViewClient(new HelloWebViewClient());
     }
 
+    public void initPos() {
+        PowerUtils.powerOnOrOff (1, "1");
+        PosFactory.registerCommunicateDriver (this, new SerialDriver ()); // 注册串口类 Register serial driver
+        mPosApi=PosFactory.getPosDevice (); // 获取打印机实例 get printer driver
+        mPosApi.setPrintEventListener (onPrintEventListener);
+        mPosApi.openDev ("/dev/ttyS2", 115200, 0);
+        mPosApi.initPos (); // 初始化打印机 init printer
+    }
+
+
+    public OnPrintEventListener onPrintEventListener=new OnPrintEventListener () {
+
+        String message = "Printer Status";
+        @Override
+        public void onPrintState(int state) {
+            AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
+            AlertDialog alertDialog = builder.create();
+            switch (state) {
+                case BarCode.ERR_POS_PRINT_SUCC:
+                    message = "Print success";
+                    break;
+                case BarCode.ERR_POS_PRINT_FAILED:
+                    message = "Error. Print failed.";
+                    break;
+                case BarCode.ERR_POS_PRINT_HIGH_TEMPERATURE:
+                    message = "Error. High temperature.";
+                    break;
+                case BarCode.ERR_POS_PRINT_NO_PAPER:
+                    message = "Error. No paper.";
+                    break;
+                case 4:
+
+                    break;
+            }
+            alertDialog.setTitle("Printer Status");
+            alertDialog.setMessage(message);
+//            alertDialog.setButton(DialogInterface.BUTTON_POSITIVE, "OK", new DialogInterface.OnClickListener() {
+//                @Override
+//                public void onClick(DialogInterface dialog, int which) {
+//                    // Ignore SSL certificate errors
+//                    isSSLErrorDialogShown = true;
+//                    handler.proceed();
+//                }
+//            });
+//
+//            alertDialog.setButton(DialogInterface.BUTTON_NEGATIVE, "Cancel", new DialogInterface.OnClickListener() {
+//                @Override
+//                public void onClick(DialogInterface dialog, int which) {
+//
+//                    handler.cancel();
+//                }
+//            });
+            alertDialog.show();
+        }
+    };
+
+    public void print_barcode() {
+        int height=80;
+        BarCodeBean barCodeBean=new BarCodeBean ();
+        barCodeBean.setConcentration (mConcentration);
+        barCodeBean.setHeight (height);
+        barCodeBean.setWidth (2);// 条码宽度1-4; Width value 1 2 3 4
+        barCodeBean.setText ("987654321012");
+        //barCodeBean.setText ("2S_201910140122126");
+        // barCodeBean.setText ("12345%$()ABcdq");
+        barCodeBean.setBarType (BarCode.CODE128);
+        mPosApi.addBarCode (barCodeBean, ALIGN_MODE.ALIGN_CENTER);
+        mPosApi.addFeedPaper (true, 2);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume ();
+        mPosApi.resume ();
+        PowerUtils.powerOnOrOff (1, "1");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause ();
+        mPosApi.stop ();
+        PowerUtils.powerOnOrOff (1, "0");
+    }
+
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy ();
+        if (mPosApi != null) {
+            mPosApi.closePos ();
+            mPosApi.closeDev ();
+            PosFactory.Destroy ();
+        }
+        PowerUtils.powerOnOrOff (1, "0");
+    }
+
 
     private class HelloWebViewClient extends WebViewClient {
         @Override
@@ -80,13 +196,15 @@ public class MainActivity extends AppCompatActivity {
             mProgressDialog.dismiss();
             view.setVisibility(View.VISIBLE);
             super.onPageFinished(view, url);
+            print_barcode();
+            mPosApi.printStart ();
         }
 
         @Override
         public void onReceivedSslError(WebView view, final SslErrorHandler handler, SslError error) {
             if (!isSSLErrorDialogShown) {
-                AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
-                AlertDialog alertDialog = builder.create();
+                    AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
+                    AlertDialog alertDialog = builder.create();
                 String message = "SSL Certificate error.";
                 switch (error.getPrimaryError()) {
                     case SslError.SSL_UNTRUSTED:
@@ -130,50 +248,50 @@ public class MainActivity extends AppCompatActivity {
     }
 
 
-    private void startWebView(String url) {
-
-        WebSettings settings = webView.getSettings();
-
-        settings.setJavaScriptEnabled(true);
-        webView.setScrollBarStyle(View.SCROLLBARS_OUTSIDE_OVERLAY);
-
-        webView.getSettings().setBuiltInZoomControls(true);
-        webView.getSettings().setUseWideViewPort(true);
-        webView.getSettings().setLoadWithOverviewMode(true);
-        webView.getSettings().setAppCacheEnabled(true);
-        webView.getSettings().setUseWideViewPort(false);
-        webView.getSettings().setSupportZoom(false);
-
-        progressDialog = new ProgressDialog(this);
-        progressDialog.setMessage("Loading...");
-        progressDialog.show();
-
-        webView.setWebViewClient(new WebViewClient() {
-            @Override
-            public boolean shouldOverrideUrlLoading(WebView view, String url) {
-                view.loadUrl(url);
-                return true;
-            }
-
-            @Override
-            public void onPageFinished(WebView view, String url) {
-                if (progressDialog.isShowing()) {
-                    progressDialog.dismiss();
-                }
-            }
-
-            @Override
-            public void onReceivedError(WebView view, int errorCode, String description, String failingUrl) {
-                Toast.makeText(MainActivity.this, "Error:" + description, Toast.LENGTH_SHORT).show();
-
-            }
-
-            @Override
-            public void onReceivedSslError(WebView view, SslErrorHandler handler, SslError error) {
-//                super.onReceivedSslError(view, handler, error);
-                handler.proceed();
-            }
-        });
-        webView.loadUrl(url);
-    }
+//    private void startWebView(String url) {
+//
+//        WebSettings settings = webView.getSettings();
+//
+//        settings.setJavaScriptEnabled(true);
+//        webView.setScrollBarStyle(View.SCROLLBARS_OUTSIDE_OVERLAY);
+//
+//        webView.getSettings().setBuiltInZoomControls(true);
+//        webView.getSettings().setUseWideViewPort(true);
+//        webView.getSettings().setLoadWithOverviewMode(true);
+//        webView.getSettings().setAppCacheEnabled(true);
+//        webView.getSettings().setUseWideViewPort(false);
+//        webView.getSettings().setSupportZoom(false);
+//
+//        progressDialog = new ProgressDialog(this);
+//        progressDialog.setMessage("Loading...");
+//        progressDialog.show();
+//
+//        webView.setWebViewClient(new WebViewClient() {
+//            @Override
+//            public boolean shouldOverrideUrlLoading(WebView view, String url) {
+//                view.loadUrl(url);
+//                return true;
+//            }
+//
+//            @Override
+//            public void onPageFinished(WebView view, String url) {
+//                if (progressDialog.isShowing()) {
+//                    progressDialog.dismiss();
+//                }
+//            }
+//
+//            @Override
+//            public void onReceivedError(WebView view, int errorCode, String description, String failingUrl) {
+//                Toast.makeText(MainActivity.this, "Error:" + description, Toast.LENGTH_SHORT).show();
+//
+//            }
+//
+//            @Override
+//            public void onReceivedSslError(WebView view, SslErrorHandler handler, SslError error) {
+////                super.onReceivedSslError(view, handler, error);
+//                handler.proceed();
+//            }
+//        });
+//        webView.loadUrl(url);
+//    }
 }
---
+++ src/main/java/com/app/example/utils/PrintUtils.java
@@ -0,0 +1,137 @@
+package com.app.example.utils;
+
+import android.content.Context;
+import android.printer.sdk.util.LogUtils;
+import android.util.Log;
+
+import java.util.Locale;
+
+public class PrintUtils {
+
+
+    public static boolean isZh(Context context) {
+        Locale locale = context.getResources().getConfiguration().locale;
+        String language = locale.getLanguage();
+        if (language.endsWith("zh"))
+            return true;
+        else
+            return false;
+    }
+
+
+    public static String LanguageChange(Context context) {
+        String str = "";
+        if (!isZh(context)) {
+            str = textEnContent();
+        } else {
+            str = textZhContent();
+        }
+        return str;
+    }
+
+    public static String textEnContent() {
+        StringBuilder sb = new StringBuilder ();
+
+        sb.append("  The credentials of cashier      ");
+        sb.append("\n");
+        sb.append("Time   : ");
+        sb.append("2016-11-15     16:00");
+        sb.append("\n");
+        sb.append("The operator:admin");
+        sb.append("\n");
+        sb.append("The receipt number：1234567890");
+        sb.append("\n");
+        sb.append("Number  Quantity  Price  Discount  Subtotal");
+        sb.append("\n");
+        sb.append("-----------------------------");
+        sb.append("\n");
+        sb.append("AM126   1  1200  0   1200");
+        sb.append("\n");
+        sb.append("AM127   1  1300  0   1300");
+        sb.append("\n");
+        sb.append("AM128   1  1400  0   1400");
+        sb.append("\n");
+
+        sb.append("-----------------------------");
+        sb.append("\n");
+        sb.append("Total sales: 3 ");
+        sb.append("\n");
+        sb.append("Total(RMB): 3900");
+        sb.append("\n");
+        sb.append("Actual amount(RMB): 3900");
+        sb.append("\n");
+        sb.append("Change(RMB): 0");
+        sb.append("\n");
+
+        sb.append("-----------------------------");
+        sb.append("\n");
+        sb.append("Mode of payment: WeChat Pay ");
+        sb.append("\n");
+        sb.append("Welcome to come again！");
+        sb.append("\n");
+        sb.append("Please keep the receipt.");
+        sb.append("\n");
+        sb.append("-----------------------------");
+
+        LogUtils.d("Length", "data length：" + sb.toString().length());
+        return sb.toString();
+    }
+
+    public static String textZhContent() {
+        Log.e ("aa", "textZhContent: ");
+        StringBuilder sb = new StringBuilder ();
+        sb.append("        收 银 凭 据                 ");
+        sb.append("\n");
+        sb.append("时间   : ");
+        sb.append("2016-11-15     16:00");
+        sb.append("\n");
+        sb.append("操作员:admin");
+        sb.append("\n");
+        sb.append("收据单号：1234567890");
+        sb.append("\n");
+        sb.append("编号  数量  单价  折扣  小计");
+
+        sb.append("\n");
+			/*
+			sb.append("下划线——————————————————\n");
+
+			sb.append("横线--------------------\n");
+			sb.append("横线---------下划线————————\n");
+			sb.append("------------------------\n");
+			sb.append("——————————————————————————\n");
+			sb.append("=== === === = = =   =======\n");
+			sb.append("+++++++----————............\n");
+			sb.append(".........................\n");
+			sb.append(".|||||//////||||||\n");
+            */
+        sb.append("-----------------------------");
+        sb.append("\n");
+        sb.append("AM126   1  1200  0   1200");
+        sb.append("\n");
+        sb.append("AM127   1  1300  0   1300");
+        sb.append("\n");
+        sb.append("AM128   1  1400  0   1400");
+        sb.append("\n");
+
+        sb.append("-----------------------------");
+        sb.append("\n");
+        sb.append("共销售数量: 3 ");
+        sb.append("\n");
+        sb.append("售价合计(RMB): 3900");
+        sb.append("\n");
+        sb.append("实收金额(RMB): 3900");
+        sb.append("\n");
+        sb.append("找零金额(RMB): 0");
+        sb.append("\n");
+
+        sb.append("-----------------------------");
+        sb.append("\n");
+        sb.append("支付方式: 微信支付 ");
+        sb.append("\n");
+        sb.append("欢迎下次光临    请保留好小票！");
+        sb.append("\n");
+        sb.append("-----------------------------");
+        LogUtils.d("Length", "data length：" + sb.toString().length());
+        return sb.toString();
+    }
+}
